07e926d599ee280180a63d07b77e25d844fc21d2
Michael Cabana <mikeycabana@gmail.com>
2020-08-11 23:14:27 -0400

    fixed ssr issue with theme toggle

--- src/components/theme-toggle.js
+++ src/components/theme-toggle.js
@@ -1,32 +1,52 @@
-import React, { useState } from "react"
+import React from "react"
 import Switch from "./switch"
 
-const ThemeToggle = () => {
-  const localValue = localStorage.getItem("data-theme") === "true"
+class ThemeToggle extends React.Component {
+  storage
+  value;
 
-  const setThemeInStorage = value => {
-    localStorage.setItem("data-theme", value)
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: false,
+    }
   }
 
-  const setThemeOnDocument = value => {
+  componentDidMount() {
+    this.storage = window.localStorage
+    this.value = this.toBoolean(this.storage.getItem("data-theme"))
+    this.setState({ value: this.value })
+    this.setThemeOnDocument(this.value)
+  }
+
+  toBoolean(str) {
+    return str === "true" ? true : false
+  }
+
+  setThemeInStorage(value) {
+    if (this.storage) {
+      this.storage.setItem("data-theme", value)
+    }
+    this.value = value;
+  }
+
+  setThemeOnDocument(value) {
     document.documentElement.setAttribute(
       "data-theme",
       value ? "dark" : "light"
     )
+    this.value = value;
   }
 
-  const [value, setValue] = useState(() => {
-    setThemeOnDocument(localValue)
-    return localValue
-  })
-
-  const toggle = () => {
-    setValue(!value)
-    setThemeOnDocument(!value)
-    setThemeInStorage(!value)
+  toggle() {
+    this.setState({ value: !this.value })
+    this.setThemeOnDocument(!this.state.value)
+    this.setThemeInStorage(!this.state.value)
   }
 
-  return <Switch isOn={value} handleToggle={toggle} />
+  render() {
+    return (<Switch isOn={this.value} handleToggle={() => this.toggle()} />)
+  }
 }
 
 export default ThemeToggle
