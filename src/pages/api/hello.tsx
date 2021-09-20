2396221c5057c3f7642e3f1cbc9e6d7e04b6b145
mikecabana <mikeyc343@gmail.com>
2021-09-20 10:38:46 -0400

    added sentry

--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { withSentry } from '@sentry/nextjs';
 
 interface Data {
 	name: string;
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 	res.status(200).json({ name: 'John Doe' });
-}
+};
+
+export default withSentry(handler);
