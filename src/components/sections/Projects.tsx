12c07ef3d605f8953ac89a08d06aab40d0158094
mikecabana <mikeyc343@gmail.com>
2023-10-23 21:27:15 -0400

    updated projects

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,81 +1,104 @@
 import { FC } from 'react';
 import { ProjectCard } from './ProjectCard';
 
-const projects = [
+type ProjectItem = {
+    label: string;
+    description: string;
+    extLink?: string;
+    tags?: string[];
+    badges?: string[];
+    disclaimer?: string;
+}
+
+const projects: ProjectItem[] = [
+	{
+		label: 'Relinkr.io',
+		description:
+			'A lightweight tool for shortening urls that are just too long. Complete with click tracking and analytics.',
+		extLink: 'https://relinkr.io',
+		tags: ['T3Stack', 'NextJs', 'TailwindCSS', 'TRPc', 'NextAuth'],
+	},
+	{
+		label: 'Pooch MD Chatbot',
+		description:
+			"An AI enhanced chatbot that can answer any questions about your puppy. It's like your virtual veterinary assistant.",
+		extLink: 'https://github.com/mikecabana/pooch-md',
+		tags: ['OpenAI', 'Langchain', 'Typescript', "NodeJs"],
+	},
+	{
+		label: 'Spotify Now Playing Widget',
+		description:
+			"A tiny widget that uses the Spotify API to display your currently playing track. I'm using it to display what song I'm listening to while streaming on Twitch 🎮.",
+		tags: ['Spotify', 'NextJs', 'Typescript', 'SCSS', 'NextAuth'],
+	},
+	{
+		label: 'RPM',
+		badges: ['WIP'],
+		description:
+			'RPM is a platform to help owners manage their rental properties. Owners are empowered to keep track of their tenants, leases and properties while tenants manage rent online and maintain communication with their concierge or building manager.',
+		tags: ['NextJs', 'Typescript', 'TailwindCSS', 'Prisma', 'NextAuth'],
+		disclaimer:
+			'This is a small passion project and is still a work in progress. If you have any questions feel free to get in touch.',
+	},
+	{
+		label: 'IEG America Website',
+		extLink: 'https://ieg-america.com',
+		description:
+			'The main website for IEG America built from the ground up with modern design and a restructured content layout. My first foray into NextJs as a framework and headless wordpress.',
+		tags: ['NextJs', 'Typescript', 'SCSS'],
+	},
     {
-        label: 'VS Concept Audio Visuel',
-        description:
-            'Andrew and Nic from VSCAV aim to provide full home automation services to the masses. They want to help break the barrier of entry and clear up any reservatioins about smart tech in the home.',
-        extLink: 'https://vscav.com',
-        tags: ['Gatsby', 'Bootstrap', 'Jquery'],
-    },
-    {
-        label: 'Lessee',
-        badges: ['WIP'],
-        description:
-            'Lessee is web app built to help property owners manage their rental properties. Owners will be able to keep track of their properties, tenants and leases while tenants will also be able to use the app to pay rent online and maintain a channel of communication with their concierge or building manager.',
-        tags: ['Next.js', 'Typescript', 'TailwindCSS', 'Prisma'],
-        disclaimer:
-            'This is a passion project and is still a work in progress. If you have any questions feel free to get in touch.',
-    },
-    {
-        label: 'Spotify Now Playing Widget',
-        description:
-            'A tiny widget that uses the Spotify API to display your currently playing track. I’m using it to display what song I’m listening to while streaming on Twitch 🎮.',
-        tags: ['Spotify', 'Next.js', 'Typescript', 'SCSS'],
-    },
-    {
-        label: 'IEG America Website',
-        extLink: 'https://ieg-america.com',
-        description: 'The main website for IEG America built from the ground up with modern design and a restructured content layout.',
-        tags: ['Next.js', 'Typescript', 'SCSS'],
-    },
-    {
-        label: 'IEG America Portal',
-        description: 'An operations management portal that provides analytics, dashboards and reports generation.',
-        tags: ['Angular', 'Typescript', 'SCSS', 'Material UI'],
-    },
+		label: 'VS Concept Audio Visuel',
+		description:
+			'Andrew and Nic from VSCAV aim to provide full home automation services to the masses. They want to help break the barrier of entry and clear up any reservatioins about smart tech in the home.',
+		// extLink: 'https://vscav.com',
+		tags: ['Gatsby', 'Bootstrap', 'JQuery'],
+	},
 ];
 
 interface ProjectsProps {}
 
 export const Projects: FC<ProjectsProps> = ({}) => {
-    // todo
-    // split the array in 2 (floor) one for each column
 
-    const midway = Math.floor(projects.length / 2);
+	const colA: ProjectItem[] = [];
+	const colB: ProjectItem[] = [];
 
-    const colA = projects.slice(0, midway);
-    const colB = projects.slice(midway);
+    projects.forEach((p, i) => {
+        if (i % 2 === 0) {
+            colA.push(p);
+        } else {
+            colB.push(p);
+        }
+    })
 
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-start">
-            <div className="grid grid-cols-1 gap-12">
-                {colA.map((p, i) => (
-                    <ProjectCard
-                        key={i}
-                        label={p.label}
-                        description={p.description}
-                        extLink={p.extLink}
-                        tags={p.tags}
-                        badges={p.badges}
-                        disclaimer={p.disclaimer}
-                    />
-                ))}
-            </div>
-            <div className="grid grid-cols-1 gap-12 items-start md:pt-12">
-                {colB.map((p, i) => (
-                    <ProjectCard
-                        key={i}
-                        label={p.label}
-                        description={p.description}
-                        extLink={p.extLink}
-                        tags={p.tags}
-                        badges={p.badges}
-                        disclaimer={p.disclaimer}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+	return (
+		<div className='grid grid-cols-1 md:grid-cols-2 gap-8 items-start'>
+			<div className='grid grid-cols-1 gap-8'>
+				{colA.map((p, i) => (
+					<ProjectCard
+						key={i}
+						label={p.label}
+						description={p.description}
+						extLink={p.extLink}
+						tags={p.tags}
+						badges={p.badges}
+						disclaimer={p.disclaimer}
+					/>
+				))}
+			</div>
+			<div className='grid grid-cols-1 gap-8 items-start md:pt-12'>
+				{colB.map((p, i) => (
+					<ProjectCard
+						key={i}
+						label={p.label}
+						description={p.description}
+						extLink={p.extLink}
+						tags={p.tags}
+						badges={p.badges}
+						disclaimer={p.disclaimer}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
