65822c3610e12a639af93a904a3ab05771916af1
mikecabana <mikeyc343@gmail.com>
2023-10-31 21:41:29 -0400

    removed under maintenance in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,13 @@
 import type { AppProps /*, AppContext */ } from 'next/app';
 import { BaseLayout } from '../components/layouts/BaseLayout';
 import { ThemeProvider } from 'next-themes';
-import { UnderMaintenanceProvider } from '../lib/useUnderMaintenance';
 import Script from 'next/script';
 
 import '../styles/globals.scss';
-import { useRouter } from 'next/dist/client/router';
-import { useEffect } from 'react';
 
 export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
 
 function MyApp({ Component, pageProps }: AppProps) {
-
     return (
         <>
             {/* Google Tag Manager - Global base code */}
@@ -28,13 +24,11 @@ function MyApp({ Component, pageProps }: AppProps) {
                     `,
                 }}
             />
-            <UnderMaintenanceProvider>
-                <ThemeProvider attribute="class" enableSystem={true} defaultTheme="dark">
-                    <BaseLayout>
-                        <Component {...pageProps} />
-                    </BaseLayout>
-                </ThemeProvider>
-            </UnderMaintenanceProvider>
+            <ThemeProvider attribute="class" enableSystem={true} defaultTheme="dark">
+                <BaseLayout>
+                    <Component {...pageProps} />
+                </BaseLayout>
+            </ThemeProvider>
         </>
     );
 }
